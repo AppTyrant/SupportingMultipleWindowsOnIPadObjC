9d7a72f82d740bd4b7227a9a3affc16819d0d025
AppTyrant <>
2019-10-30 14:51:21 -0400

    initial commit.

---
+++ SupportingMultipleWindowsOnIPadObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SupportingMultipleWindowsOnIPadObjC
+//
+//  Created by ANTHONY CRUZ on 10/30/19.
+//  Copyright © 2019 Writes for All. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SupportingMultipleWindowsOnIPadObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  SupportingMultipleWindowsOnIPadObjC
+//
+//  Created by ANTHONY CRUZ on 10/30/19.
+//  Copyright © 2019 Writes for All. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
